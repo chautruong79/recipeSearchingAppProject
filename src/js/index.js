a24c8b7285892a62f3ebc1bc49079f23603e7e49
chautruong79 <62908290+chautruong79@users.noreply.github.com>
2020-05-20 20:16:55 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
-// Global app controller
+import Search from './models/Search';
+
+const state = {};
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
+document.querySelector('.btn search__btn').addEventListener('submit', e => {
+    e.preventDefault();
+    getInput();
+});
+document.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13 || event.which === 13)
+        getInput();
+});
+const getInput = () => {
+    // Get new query
+    const query = 'pizza';//TODO
+    //Add new search object to state
+    if (query) {
+        state.search = new Search(query);
+        //Prepare UI for results
+        
+    }
+
+};
